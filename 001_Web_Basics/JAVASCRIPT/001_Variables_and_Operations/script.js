06de47cd20e8fdd6d0c6dc2d09d40c97d3e35dd0
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2023-12-07 10:45:03 +0100

    ue1_VariablesAndOperations_finished

---
+++ 001_Web_Basics/JAVASCRIPT/001_Variables_and_Operations/script.js
@@ -0,0 +1,63 @@
+console.log("hello");
+console.log("tolle Sache");
+
+// Variables
+//let a = 7;
+//let b = 8;
+//let c = a + b;
+
+//console.log("A hat den Wert " + a);
+
+//a = 10;
+
+//console.log("A hat den Wert " + a);
+
+//console.log("a + b = " + c);
+
+//Rechnungsarten
+// *, /, +, -
+
+//c = b - a;
+//console.log("b - a = " + c);
+
+
+
+
+//Aufgabe
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+console.log(a + b + c);
+// (a + b) / c
+console.log((a + b) / c);
+// c -a + b
+console.log(c - a + b);
+// a + b + c * 3
+console.log(a + b + c * 3);
+// (a + b + c)/4
+console.log((a + b + c) / 4);
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+
+// Variablen
+a = 3;
+b = a + 3;
+c = c - 3;
+
+// a + b + c
+console.log(a + b + c);
+// (a + b) / c
+console.log((a + b) / c);
+// c -a + b
+console.log(c - a + b);
+// a + b + c * 3
+console.log(a + b + c * 3);
+// (a + b + c)/4
+console.log((a + b + c) / 4);
