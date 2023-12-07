72a2c4538f528f63522bcd26b65e65a4b2d02679
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2023-12-07 11:02:22 +0100

    ue2_StringsAndNumbers_finished

---
+++ 001_Web_Basics/JAVASCRIPT/002_Strings_and_Numbers/script.js
@@ -0,0 +1,35 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+//+, -. *, /
+
+let c = 7.4;
+let d = 7.5;
+
+let result = c + d;
+console.log(result);
+
+//Texts STRINGS
+
+let name = "Sebastian";
+console.log(name);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
+
+
+//Task 1
+let e = 17.4;
+let f = 3.9;
+
+let ef = e / f;
+
+console.log("The result of " + e + " divided by " + f + " is " + ef);
+
+//Task 2
+let firstname = "Sebastian";
+let secondname = "Lukas";
+let lastname = "Stecher";
+
+console.log("Ich heiße " + firstname + " " + secondname + " " + lastname);
