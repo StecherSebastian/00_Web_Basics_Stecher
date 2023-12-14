a26ea23ea5fad5a226c5a4d603878d6e0ccb31da
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2023-12-14 10:51:48 +0100

    ue4_Loops_PartTwo_finished

--- 001_Web_Basics/JAVASCRIPT/004_Loops/script.js
+++ 001_Web_Basics/JAVASCRIPT/004_Loops/script.js
@@ -1,7 +1,7 @@
 console.log("test");
 
 let a = 7;
-let b = 4-3;
+let b = 4 - 3;
 let name = "Sebastian";
 
 console.log(name + " " + a + b);
@@ -9,8 +9,49 @@ console.log(name + " " + a + b);
 
 //Schleife
 
-for (let count = 0; count < 10; count++){
+for (let count = 10; count > 0; count--) {
     console.log(count + " Sebastian");
 }
 
 console.log("Fertig");
+
+
+// Zähle von 7 bis 45
+for (i = 7; i < 45; i++) {
+    console.log(i);
+}
+console.log("Finished counting from 7 to 45");
+
+// Zähle von 9 bis 99
+for (i = 9; i < 99; i++) {
+    console.log(i);
+}
+console.log("Finished counting from 9 to 99");
+
+// Zähle rückwärts von 10 bis 0
+for (i = 10; i > 0; i--) {
+    console.log(i);
+}
+console.log("Finished counting down from 10 to 0");
+
+// Zähle rückwärts von 1234 bis 1207
+for (i = 1234; i > 1207; i--) {
+    console.log(i);
+}
+console.log("Finished counting down from 1234 to 1207");
+
+// Gib Deinen Namen genau 8 mal aus
+for (i = 0; i < 8; i++) {
+    console.log("Sebastian Lukas Stecher");
+}
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (i = 0; i  < 10; i++) {
+    console.log(i);
+}
+
+console.log("Sebastian Lukas Stecher");
+
+for (i = 10; i  > 0; i--) {
+    console.log(i);
+}
