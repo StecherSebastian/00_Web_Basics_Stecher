f6446494fe47ac8d2bd0bd461efab44eab4e1baf
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2023-12-14 10:36:52 +0100

    ue4_Loops_PartOne_finished

---
+++ 001_Web_Basics/JAVASCRIPT/004_Loops/script.js
@@ -0,0 +1,16 @@
+console.log("test");
+
+let a = 7;
+let b = 4-3;
+let name = "Sebastian";
+
+console.log(name + " " + a + b);
+
+
+//Schleife
+
+for (let count = 0; count < 10; count++){
+    console.log(count + " Sebastian");
+}
+
+console.log("Fertig");
