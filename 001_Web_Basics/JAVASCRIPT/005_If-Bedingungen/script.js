a10a2de7ed1c565b131db99d7de15016bc0723c7
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2023-12-14 11:13:05 +0100

    ue5_If-Bedingungen_finished

---
+++ 001_Web_Basics/JAVASCRIPT/005_If-Bedingungen/script.js
@@ -0,0 +1,42 @@
+let zahl = 44;
+
+if (zahl <= 20) {
+    console.log(zahl);
+} else if (zahl < 50) {
+    console.log("kleiner 50 ");
+} else {
+    console.log("else");
+}
+
+
+// erstelle eine Variable number
+let number = 60;
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number < 20) {
+    console.log("Mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+else if (number > 20 && number < 50) {
+    console.log("Medium");
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+else {
+    console.log("Large");
+}
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (number <= 7) {
+    console.log("Mini");
+}
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+else if (number > 7 && number < 90) {
+    console.log("Medium");
+}
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+else {
+    console.log("Large");
+}
