095e35c3b2a30c3917123a2da7d624a1117d7e9d
StecherSebastian <sebastian.stecher@student.htldornbirn.at>
2023-12-21 09:58:41 +0100

    ue6_1_NodeJS_successfull_Implemted

---
+++ 001_Web_Basics/JAVASCRIPT/006_NodeJS/script.js
@@ -0,0 +1,17 @@
+//NodeJS
+// cd ..            Einen Ordner hoch
+// cd demo          In den Ordner dem
+// pwd              Zeigt den aktiven Ordner an
+// node script.js   führt script.js im aktiven Ordner aus
+// ls               Zeigt alle Files im aktuellen Directory
+// cd J (TAB)       Schlägt den passenden Ordenr vor
+// Pfeil nach oben  zuletzt ausgeführte Befehle auswählen
+
+
+let zahl = 5;
+
+if (zahl == 6) {
+    console.log("Die Zahl hat den Wert 6");
+} else {
+    console.log("Die Zahl hat nicht den Wert 6");
+}
