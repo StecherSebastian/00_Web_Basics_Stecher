757dd65e14920926eaa5645796df2caf684b3fd1
Sebastian <sebastian.stecher@student.htldornbirn.at>
2024-04-04 10:13:31 +0200

    ue12_InputFromShell_finished

---
+++ 001_Web_Basics/JAVASCRIPT/012_InputFromShell/script.js
@@ -0,0 +1,21 @@
+import { log } from "console";
+import { resolve } from "path";
+import { createInterface } from "readline";
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const readLineAsync = () => {
+    return new Promise((resolve) => {
+        readline.question("", (userRes) => {
+            resolve(userRes);
+
+        });
+    });
+};
+
+console.log("Enter your name:");
+let inputOfUser = await readLineAsync();
+console.log("Hello " + inputOfUser + "!");
+
+readline.close();
