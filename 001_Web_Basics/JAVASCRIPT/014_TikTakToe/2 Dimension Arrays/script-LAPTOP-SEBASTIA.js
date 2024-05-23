169779c8636c624adc36b4b1453f939d8f340b7b
Sebastian <sebastian.stecher@student.htldornbirn.at>
2024-05-23 10:31:17 +0200

    ue14_TikTakToe_improved

---
+++ 001_Web_Basics/JAVASCRIPT/014_TikTakToe/2 Dimension Arrays/script-LAPTOP-SEBASTIA.js
@@ -0,0 +1,29 @@
+// 2 Dimensionale Arrays - verschachtelte Arrays
+let field2D = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+console.log(field2D[2][2]);
+
+// Test 3D-Arrays
+let field3d = [
+    [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ],
+    [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ],
+    [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ]
+]
+
+console.log(field3d[2][1][0]);
---
+++ 001_Web_Basics/JAVASCRIPT/014_TikTakToe/TikTakToe/script.js
@@ -0,0 +1,257 @@
+import { userInfo } from "os";
+import { resolve } from "path";
+import { createInterface } from "readline";
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const readLineAsync = () => {
+    return new Promise((resolve) => {
+        readline.question("", (userRes) => {
+            resolve(userRes);
+
+        });
+    });
+};
+
+
+// Variable die das Spielfeld enthält
+let field_2D = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+// Variable zum zwischenspeichern des Users Inputs
+let userInput;
+let x;
+let y;
+
+// Funktion die das Spielfeld zurücksetzt
+function reset() {
+    field_2D = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+}
+
+// Funktion die das Spielfeld im Terminal ausgibt
+function printField() {
+    // Ausgeben der 3 geschachtelten Arrays als Zeilen.
+    for (let row = 0; row < field_2D.length; row++) {
+        let actualRow = field_2D[row];
+        let output = " ";
+        // Strukturieren des Spielfelds
+        for (let col = 0; col < actualRow.length; col++) {
+            if (col == 0) {
+                output += "| ";
+            };
+            // Einfüllen von X & O
+            switch (actualRow[col]) {
+                case 0:
+                    output += " ";
+                    break;
+                case 1:
+                    output += "X";
+                    break;
+                case 2:
+                    output += "O";
+                    break;
+            }
+            // Für Strukturierung
+            output += " | ";
+        };
+        console.log(output);
+    };
+};
+
+// Funktion die überprüft ob jemand gewonnen hat
+function checkIfWon() {
+    // Geht die Reihen durch
+    for (let row = 0; row < field_2D.length; row++) {
+        // Überprüft ob jemand in der aktuellen Reihe gewonnen hat
+        if (field_2D[row][0] != 0 && (field_2D[row][0] == field_2D[row][1] && field_2D[row][0] == field_2D[row][2])) {
+            // Überprüft ob X & O gewonnen hat
+            if (field_2D[row][0] == 1) {
+                console.log("X has won!");
+                reset();
+                return true;
+            } else if (field_2D[row][0] == 2) {
+                console.log("O has won!");
+                reset();
+                return true;
+            };
+        };
+    };
+
+    // Geht die Spalten durch
+    for (let col = 0; col < field_2D[0].length; col++) {
+        // Überprüft ob jemand in der aktuellen Spalte gewonnen hat
+        if (field_2D[0][col] != 0 && (field_2D[0][col] == field_2D[1][col] && field_2D[0][col] == field_2D[2][col])) {
+            // Überprüft ob X & O gewonnen hat
+            if (field_2D[0][col] == 1) {
+                console.log(" ");
+                console.log("X has won!");
+                console.log(" ");
+                reset();
+                return true;
+            } else if (field_2D[0][col] == 2) {
+                console.log(" ");
+                console.log("O has won!");
+                console.log(" ");
+                reset();
+                return true;
+            };
+        };
+    };
+
+    // Übeprüft ob jemand diagonal von Obenlinks bis Untenrechts gewonnen hat
+    if (field_2D[0][0] != 0 && (field_2D[0][0] == field_2D[1][1] && field_2D[0][0] == field_2D[2][2])) {
+        // Überprüft ob X & O gewonnen hat
+        if (field_2D[0][0] == 1) {
+            console.log(" ");
+            console.log("X has won!");
+            console.log(" ");
+            reset();
+            return true;
+        } else if (field_2D[0][0] == 2) {
+            console.log(" ");
+            console.log("O has won!");
+            console.log(" ");
+            reset();
+            return true;
+        };
+    }
+    // Übeprüft ob jemand diagonal von Untenlinks bis Obenrechts gewonnen hat
+    else if (field_2D[2][0] != 0 && (field_2D[2][0] == field_2D[1][1] && field_2D[2][0] == field_2D[0][2])) {
+        // Überprüft ob X & O gewonnen hat
+        if (field_2D[2][0] == 1) {
+            console.log(" ");
+            console.log("X has won!");
+            console.log(" ");
+            reset();
+            return true;
+        } else if (field_2D[2][0] == 2) {
+            console.log(" ");
+            console.log("O has won!");
+            console.log(" ");
+            reset();
+            return true;
+        };
+    };
+};
+
+async function game() {
+    console.log("-----------------");
+    console.log("Start Game - y/n:");
+    console.log(" ");
+    userInput = await readLineAsync();
+
+    if (userInput == "y") {
+        console.log(" ");
+        console.log("Which player should start - x/o:");
+        console.log(" ");
+        userInput = await readLineAsync();
+        if (userInput == "x") {
+            console.log(" ");
+            printField();
+            playerX();
+        } else if (userInput == "o") {
+            console.log(" ");
+            printField();
+            playerO();
+        } else {
+            console.log(" ");
+            console.log("# Invalid Input #");
+            console.log(" ");
+            game();
+        }
+    } else {
+        console.log(" ");
+        console.log("# Stopped Game #");
+        console.log(" ");
+        readline.close();
+    };
+};
+
+// Funktion für Spieler X
+async function playerX() {
+
+    console.log(" ");
+    console.log("Player X is active:");
+    console.log("Input Schema - 0 0");
+    console.log(" ");
+
+    userInput = await readLineAsync();
+
+    if (userInput)
+
+        userInput = userInput.split(" ");
+    x = parseInt(userInput[0]);
+    y = parseInt(userInput[1]);
+
+    if (x == undefined || y == undefined || isNaN(x) || isNaN(y) || x > 2 || y > 2 || x < 0 || y < 0 || field_2D[x][y] == 1 || field_2D[x][y] == 2) {
+        console.log(" ");
+        console.error("# Invalid Input try again #");
+        console.log(" ");
+        playerX();
+
+    } else if (field_2D[x][y] == 0) {
+
+        field_2D[x][y] = 1;
+        console.log(" ");
+        printField();
+
+        if (checkIfWon() == true) {
+
+            game();
+
+        } else {
+
+            playerO();
+
+        }
+    }
+}
+
+// Funktion für Spieler O
+async function playerO() {
+
+    console.log(" ");
+    console.log("Player O is active:");
+    console.log("Input Schema - 0 0");
+    console.log(" ");
+
+    userInput = await readLineAsync();
+
+    userInput = userInput.split(" ");
+    x = parseInt(userInput[0]);
+    y = parseInt(userInput[1]);
+
+    if (x == undefined || y == undefined || isNaN(x) || isNaN(y) || x > 2 || y > 2 || x < 0 || y < 0 || field_2D[x][y] == 1 || field_2D[x][y] == 2) {
+
+        console.log(" ");
+        console.error("# Invalid Input try again #");
+        console.log(" ");
+        playerO();
+
+    } else if (field_2D[x][y] == 0) {
+
+        field_2D[x][y] = 2;
+        console.log(" ");
+        printField();
+
+        if (checkIfWon() == true) {
+
+            game();
+
+        } else {
+
+            playerX();
+
+        }
+    }
+}
+
+game();
